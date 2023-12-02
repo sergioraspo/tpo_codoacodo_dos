70abf1e5c5fa2e044a27444b6cd72eccae280b92
sergioraspo <sergioraspo@gmail.com>
2023-12-02 18:51:23 -0300

    second repo

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    
+    title = models.CharField(max_length= 200, verbose_name="Titulo")
+    director = models.CharField(max_length= 150, verbose_name="Director")
+    release_date = models.DateField(null=True, verbose_name="Fecha de lanzamiento")
+
+def __str__(self):
+    return self.title
+   
+
+
--- cac_movies_bkn_24/urls.py
+++ cac_movies_bkn_24/urls.py
@@ -21,5 +21,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls'))
+    path('api/', include('app.urls')),
 ]
+
